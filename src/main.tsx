0ed5cd6e5b8befb6219f4ae6998e398d9895377b
fl3ur_1nestim4ble <yebouetephraim@gmail.com>
2024-09-04 18:56:59 -0400

    Set up basic routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Routes/Home'
+import About from './Routes/About';
+import Create from './Routes/Create';
+import HowToUse from './Routes/HowToUse';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/how-to-use" element={<HowToUse />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
