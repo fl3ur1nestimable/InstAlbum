c756a0cb7977463f38dff9962ef53fc7385267ec
fl3ur_1nestim4ble <yebouetephraim@gmail.com>
2024-09-06 19:03:53 -0400

    About page start, responsive

--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -1,23 +1,51 @@
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import Card from '../Components/Card';
+import { useTranslation } from 'react-i18next';
 
 const About = () => {
+  const { t } = useTranslation();
   return (
     <div>
       <Header />
-            <p className="text-center text-4xl">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
-            <p className="text-center text-4xl mt-20">About InstAlbum</p>
+      <div className='p-4 xl:pr-40 xl:pl-40'>
+        <section className='flex flex-col-reverse md:flex-row gap-10 items-center md:items-stretch md:justify-between mt-4 p-4'>
+          <div className='flex flex-col items-center md:items-start gap-10 text-center md:text-left'>
+            <h2 className='font-bold text-red text-4xl lg:text-6xl text-center'>
+              {t('about.section1.title')}
+            </h2>
+            <p className='text-black text-xl '>
+              {t('about.section1.text')}
+            </p>
+          </div>
+          <img
+            src="./imgs/polaroid.png"
+            alt="upload_img"
+            className='w-60 rounded-md h-60 object-cover'
+          />
+        </section>
+        <section className='flex flex-col-reverse md:flex-row-reverse gap-10 items-center md:items-stretch md:justify-between mt-4 rounded-md bg-rose-pale p-4'>
+          <div className='flex flex-col items-center md:items-end gap-10 text-center md:text-right'>
+            <h2 className='font-bold text-blue-dark text-4xl lg:text-6xl text-center'>
+              {t('about.section2.title')}
+            </h2>
+            <p className='text-black text-xl '>
+              {t('about.section2.text')}
+            </p>
+          </div>
+          <img
+            src="./imgs/polaroidPhotos.png"
+            alt="upload_img"
+            className='w-60 rounded-md h-60 object-cover'
+          />
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+      </div>
       <Footer />
     </div>
   )
